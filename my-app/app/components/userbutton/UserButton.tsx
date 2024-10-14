4bdfcb08228871e53ce39d49c5d5dc35d806c034
unknown <samuelfentie55@gmail.com>
2024-10-14 15:51:28 +0300

    'routing_and_client_side_interactivity'

---
+++ my-app/app/components/userbutton/UserButton.tsx
@@ -0,0 +1,9 @@
+'use client'
+import React from 'react'
+const UserButton = () => {
+  return (
+    <button onClick={()=>{console.log('Hello world')}}>Console</button>
+  )
+}
+
+export default UserButton
---
+++ my-app/app/users/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const users = () => {
+  return (
+    <div>
+      users 
+    </div>
+  )
+}
+
+export default users
+
